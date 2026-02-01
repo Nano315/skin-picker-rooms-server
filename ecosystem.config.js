69293ee0808d72f5c6dc8301c1884271c537fc64
Nano315 <valentin3135@gmail.com>
2026-02-01 19:32:56 +0100

    feat: add CI/CD deploy workflow + PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'skin-picker-rooms-server',
+      script: 'dist/server.js',
+      cwd: '~/Skin-Picker/skin-picker-rooms-server',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000,
+      },
+    },
+    {
+      name: 'skin-picker-rooms-server-staging',
+      script: 'dist/server.js',
+      cwd: '~/Skin-Picker/skin-picker-rooms-server-staging',
+      env: {
+        NODE_ENV: 'staging',
+        PORT: 4001,
+      },
+    },
+  ],
+};
